ce77ac1ef64a829fbf8b9bc7567d1fcc76e09993
donkeeman <doonkeeemaan@gmail.com>
2022-11-24 02:33:30 +0900

    Fix: 전역 스타일 수정

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -8,17 +8,21 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
     body, button, input {
+        text-align: center;
         font-family: 'Noto Sans KR', sans-serif;
-        font-size: 10px;
+        font-size: 14px;
         letter-spacing: -0.8px;
         color: ${colorVariants.black};
     }
     body {
         padding: 20px;
     }
-    button, button:hover, button:focus {
-        background-color: inherit;
-        cursor: pointer;
+    h1, h2 {
+        font-size: 20px;
+        font-weight: bold;
+    }
+    a, a:visited, a:hover{
+        color: inherit;
     }
 `;
 
