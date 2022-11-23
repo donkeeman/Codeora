6fe5064ebc84e32c9e9855c573f2b142738a7e5e
donkeeman <doonkeeemaan@gmail.com>
2022-11-24 02:32:49 +0900

    Feat: 메인 색 및 비활성화 색 추가

--- src/Constants/colorVariants.ts
+++ src/Constants/colorVariants.ts
@@ -1,5 +1,6 @@
 export const colorVariants = {
-    // mainColor: "rgb()",
+    mainColor: "rgb(0, 187, 144)",
+    disabledColor: "rgb(233, 229, 214)",
     black: "rgb(34, 34, 34)",
     white: "rgb(245, 245, 245)",
 };
