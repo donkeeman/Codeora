c63f13adf03a1994066cc51c620e6bb702091d00
donkeeman <doonkeeemaan@gmail.com>
2023-01-18 15:42:24 +0900

    Fix: 파이어베이스 설정 값들을 .env에 저장하도록 변경

--- src/Configs/firebase.ts
+++ src/Configs/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "codeora-421.firebaseapp.com",
-    projectId: "codeora-421",
-    storageBucket: "codeora-421.appspot.com",
-    messagingSenderId: "873041017960",
-    appId: "1:873041017960:web:d6bc115cf0f366e0030fdd",
-    measurementId: "G-0177D6YLX5",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
