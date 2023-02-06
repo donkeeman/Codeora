e4103e1191dc21a6a7b1e3f873b8679a96ddd84c
donkeeman <doonkeeemaan@gmail.com>
2023-02-06 20:35:16 +0900

    Feat: 파이어스토어에 저장했던 코드를 불러오는 기능 추가

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { useNavigate, Navigate } from "react-router-dom";
+import { useQuery } from "react-query";
 import { currentUserState } from "../Configs/atoms";
 import { useRecoilState } from "recoil";
 import { signOutUser } from "../Services/auth";
+import { db } from "../Configs/firebase";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { queryKeys } from "../Constants/queryKeys";
 
 const Main = () => {
     const [userData, setUserData] = useRecoilState(currentUserState);
     const navigate = useNavigate();
+
+    const getData = async () => {
+        if (userData) {
+            const codeQuery = query(
+                collection(db, `user/${userData.uid}/codes`),
+                orderBy("timestamp", "desc"),
+                limit(12) // 추후에 무한 스크롤 추가
+            );
+            const codeSnapshot = await getDocs(codeQuery);
+            return codeSnapshot.docs;
+        }
+    };
+
+    const { data, isLoading } = useQuery(queryKeys.codes, getData);
 
     return userData ? (
         <>
@@ -21,6 +39,14 @@ const Main = () => {
                 로그아웃
             </button>
             <button onClick={() => navigate("/write")}>글쓰기</button>
+            <ol>
+                {!isLoading &&
+                    data?.map((doc) => {
+                        return (
+                            <li key={doc.id}>{JSON.stringify(doc.data())}</li>
+                        );
+                    })}
+            </ol>
         </>
     ) : (
         <>
