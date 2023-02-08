efded8765d380d301d645922e4ba22e839e9648a
donkeeman <doonkeeemaan@gmail.com>
2023-02-08 16:44:42 +0900

    Fix: 파이어스토어에서 불러온 코드 li를 CodeCard 컴포넌트로 변경

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useNavigate, Navigate } from "react-router-dom";
-import { useQuery } from "react-query";
-import { currentUserState } from "../Configs/atoms";
 import { useRecoilState } from "recoil";
-import { signOutUser } from "../Services/auth";
-import { db } from "../Configs/firebase";
+import { useQuery } from "react-query";
 import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import CodeCard from "../Components/CodeCard";
+import { currentUserState } from "../Configs/atoms";
+import { db } from "../Configs/firebase";
 import { queryKeys } from "../Constants/queryKeys";
+import { signOutUser } from "../Services/auth";
 
 const Main = () => {
     const [userData, setUserData] = useRecoilState(currentUserState);
@@ -42,8 +43,17 @@ const Main = () => {
             <ol>
                 {!isLoading &&
                     data?.map((doc) => {
+                        const codeData = doc.data();
                         return (
-                            <li key={doc.id}>{JSON.stringify(doc.data())}</li>
+                            <li key={doc.id}>
+                                <CodeCard
+                                    title={codeData.title}
+                                    description={codeData.description}
+                                    tags={codeData.tag}
+                                    language={codeData.language}
+                                    date={codeData.timestamp.toDate()}
+                                />
+                            </li>
                         );
                     })}
             </ol>
