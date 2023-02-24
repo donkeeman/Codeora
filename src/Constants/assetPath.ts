07ca0f9230403ac74839c0817151c72c602b3fbf
donkeeman <doonkeeemaan@gmail.com>
2023-02-24 20:19:38 +0900

    Feat: 로고 추가

--- src/Constants/assetPath.ts
+++ src/Constants/assetPath.ts
@@ -1,3 +1,8 @@
+export const logoPaths = {
+    web: "/Assets/logo_web.png",
+    mobile: "/Assets/logo_mobile.png",
+};
+
 export const iconPaths = {
     google: "/Assets/btn_google_dark_normal_ios.svg",
     github: "/Assets/gitHub-Mark-Light-120px-plus.svg",
