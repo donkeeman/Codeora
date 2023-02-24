454869ab6e6fc50840a431d200b7b9462153fc31
donkeeman <doonkeeemaan@gmail.com>
2023-02-24 20:17:34 +0900

    Rename: Icon 폴더 이름을 Assets로 변경

---
+++ src/Constants/assetPath.ts
@@ -0,0 +1,4 @@
+export const iconPaths = {
+    google: "/Assets/btn_google_dark_normal_ios.svg",
+    github: "/Assets/gitHub-Mark-Light-120px-plus.svg",
+};
--- src/Constants/iconPaths.ts
+++
@@ -1,4 +0,0 @@
-export const iconPaths = {
-    google: "/Icons/btn_google_dark_normal_ios.svg",
-    github: "/Icons/gitHub-Mark-Light-120px-plus.svg",
-};
