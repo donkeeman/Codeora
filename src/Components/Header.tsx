9a1627fde217f2e10b4b0986fb9d45a6fdee6641
donkeeman <doonkeeemaan@gmail.com>
2023-02-26 23:19:32 +0900

    Feat: Header 컴포넌트에 미디어 쿼리 적용

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { colorVariants } from "../Constants/colorVariants";
@@ -7,6 +7,14 @@ import { currentUserState, persistLoginState } from "../Configs/atoms";
 import { signOutUser } from "../Services/auth";
 import { headerMaxHeight } from "../Constants/variables";
 import { logoPaths } from "../Constants/assetPath";
+import {
+    faKeyboard,
+    faRightFromBracket,
+    faRightToBracket,
+    faUserPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import IconButton from "./IconButton";
+import IconLink from "./IconLink";
 
 const HeaderWrapper = styled.header`
     position: fixed;
@@ -16,34 +24,92 @@ const HeaderWrapper = styled.header`
     height: ${headerMaxHeight}px;
     background-color: ${colorVariants.darkBlack};
     display: flex;
-    justify-content: flex-end;
-    gap: 16px;
+    justify-content: space-between;
     align-items: center;
     padding: 0 50px;
+    @media screen and (max-width: 830px) {
+        padding: 0 20px;
+    }
+    @media screen and (max-width: 540px) {
+        justify-content: center;
+        padding: 0;
+    }
 `;
 
 const Logo = styled.h1`
-    margin-right: auto;
     width: 200px;
     max-height: 100%;
     & * {
         max-width: 100%;
     }
+    .narrow {
+        display: none;
+    }
+    @media screen and (max-width: 540px) {
+        width: 120px;
+        .wide {
+            display: none;
+        }
+        .narrow {
+            display: inline;
+        }
+    }
 `;
 
-const User = styled.p`
+const NavWrapper = styled.nav`
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    @media screen and (max-width: 540px) {
+        display: none;
+        /* 메뉴가 많아진다면 햄버거 메뉴 형식으로 */
+    }
+`;
+
+const User = styled.strong`
     font-size: 18px;
+    &:after {
+        content: "님, 어서 오세요!";
+    }
+    @media screen and (max-width: 830px) {
+        &:after {
+            content: "";
+        }
+    }
+    @media screen and (max-width: 540px) {
+        display: none;
+    }
+`;
+
+const NavList = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    &.narrow {
+        display: none;
+    }
+    @media screen and (max-width: 830px) {
+        gap: 6px;
+        &.wide {
+            display: none;
+        }
+        &.narrow {
+            display: flex;
+        }
+    }
 `;
 
 const NavLink = styled(Link)`
     background-color: ${colorVariants.darkBlack};
     color: ${colorVariants.white};
     font-size: 18px;
-    padding: 8px 16px;
+    padding: 4px 16px;
     border-radius: 20px;
     border: 3px solid ${colorVariants.mainColor};
     &:hover,
     &:focus-visible {
+        outline: none;
         background-color: ${colorVariants.mainColor};
         color: ${colorVariants.black};
     }
@@ -53,12 +119,12 @@ const NavButton = styled.button`
     background-color: ${colorVariants.darkBlack};
     color: ${colorVariants.white};
     font-size: 18px;
-    padding: 8px 16px;
+    padding: 4px 16px;
     border-radius: 20px;
-    line-height: 1;
     border: 3px solid ${colorVariants.mainColor};
     &:hover,
     &:focus-visible {
+        outline: none;
         background-color: ${colorVariants.mainColor};
         color: ${colorVariants.black};
     }
@@ -67,37 +133,98 @@ const NavButton = styled.button`
 const Header = () => {
     const [userData, setUserData] = useRecoilState(currentUserState);
     const setPersistLoginData = useSetRecoilState(persistLoginState);
+    const navigate = useNavigate();
+
+    const signOutHandler = async () => {
+        setUserData(undefined);
+        setPersistLoginData(undefined);
+        await signOutUser();
+        navigate("/");
+    };
 
     return (
         <HeaderWrapper>
             <Logo>
                 <Link to="/">
                     <img
+                        className="wide"
                         src={process.env.PUBLIC_URL + logoPaths.web}
+                        alt="코더라"
+                    />
+                    <img
+                        className="narrow"
+                        src={process.env.PUBLIC_URL + logoPaths.mobile}
                         alt="코더라"
                     />
                 </Link>
             </Logo>
-            {userData ? (
-                <>
-                    <User>{userData.displayName}님, 어서 오세요!</User>
-                    <NavLink to="/write">코드 작성</NavLink>
-                    <NavButton
-                        onClick={async () => {
-                            setUserData(undefined);
-                            setPersistLoginData(undefined);
-                            await signOutUser();
-                        }}
-                    >
-                        로그아웃
-                    </NavButton>
-                </>
-            ) : (
-                <>
-                    <NavLink to="/signup">회원 가입</NavLink>
-                    <NavLink to="/signin">로그인</NavLink>
-                </>
-            )}
+            <NavWrapper>
+                {userData ? (
+                    <>
+                        <User>{userData.displayName}</User>
+                        <NavList className="wide">
+                            <li>
+                                <NavLink to="/write">
+                                    <span>코드 작성</span>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavButton onClick={signOutHandler}>
+                                    <span>로그아웃</span>
+                                </NavButton>
+                            </li>
+                        </NavList>
+                        <NavList className="narrow">
+                            <li>
+                                <IconLink
+                                    to="/write"
+                                    icon={faKeyboard}
+                                    message="코드 작성"
+                                    size="xl"
+                                />
+                            </li>
+                            <li>
+                                <IconButton
+                                    onClickFunction={signOutHandler}
+                                    icon={faRightFromBracket}
+                                    message="로그아웃"
+                                />
+                            </li>
+                        </NavList>
+                    </>
+                ) : (
+                    <>
+                        <NavList className="wide">
+                            <li>
+                                <NavLink to="/signup">
+                                    <span>회원 가입</span>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/signin">
+                                    <span>로그인</span>
+                                </NavLink>
+                            </li>
+                        </NavList>
+                        <NavList className="narrow">
+                            <li>
+                                <IconLink
+                                    to="/signup"
+                                    icon={faUserPlus}
+                                    message="회원 가입"
+                                />
+                            </li>
+                            <li>
+                                <IconLink
+                                    to="/signin"
+                                    icon={faRightToBracket}
+                                    message="로그인"
+                                />
+                            </li>
+                        </NavList>
+                    </>
+                )}
+            </NavWrapper>
         </HeaderWrapper>
     );
 };
