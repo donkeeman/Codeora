bcf0f6bb495c607a3fadd9343019e65959cd3366
donkeeman <doonkeeemaan@gmail.com>
2023-02-28 21:46:37 +0900

    Fix: 리코일으로 바꿨던 로그인 유지 정보를 로컬 스토리지에서 받아오고 있던 문제 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect } from "react";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import Loading from "./Components/Loading";
 import Header from "./Components/Header";
-import { currentUserState } from "./Configs/atoms";
+import { currentUserState, persistLoginState } from "./Configs/atoms";
 import GlobalStyle from "./Configs/GlobalStyle";
 import Router from "./Configs/Router";
 
 const App = () => {
     const setUserData = useSetRecoilState(currentUserState);
+    const persistUserData = useRecoilValue(persistLoginState);
     useEffect(() => {
-        const localUser = window.localStorage.getItem("user");
-        if (localUser) {
-            setUserData(JSON.parse(localUser));
-        }
-    }, [setUserData]);
+        setUserData(persistUserData);
+    }, [setUserData, persistUserData]);
 
     return (
         <div className="App">
