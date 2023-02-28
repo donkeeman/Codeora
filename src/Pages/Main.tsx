f921b2e660077b8bf5ace00c443a46837830c7f8
donkeeman <doonkeeemaan@gmail.com>
2023-02-28 21:25:13 +0900

    Feat: 메인 페이지에서 무한 스크롤을 이용하여 코드를 불러오는 기능 추가

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-import { useQuery } from "react-query";
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useInfiniteQuery } from "react-query";
+import {
+    collection,
+    DocumentData,
+    getDocs,
+    limit,
+    orderBy,
+    query,
+    startAfter,
+} from "firebase/firestore";
 import styled from "styled-components";
 import CodeCard from "../Components/CodeCard";
+import Spinner from "../Components/Spinner";
 import { currentUserState } from "../Configs/atoms";
 import { db } from "../Configs/firebase";
 import { queryKeys } from "../Constants/queryKeys";
+import { variables } from "../Constants/variables";
 
 const MainWrapper = styled.section`
     width: 100%;
@@ -15,71 +25,131 @@ const MainWrapper = styled.section`
 
 const CodeList = styled.ol`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(4, 230px);
     justify-content: center;
-    justify-items: center;
-    row-gap: 8px;
-    & > li {
-        width: 230px;
-        height: 250px;
+    justify-items: stretch;
+    gap: 8px;
+    @media screen and (max-width: ${variables.MAX_WIDTH}px) {
+        grid-template-columns: repeat(3, 230px);
     }
-    @media screen and (max-width: 1100px) {
-        grid-template-columns: repeat(3, 1fr);
+    @media screen and (max-width: ${variables.MEDIA_FIRST_WIDTH}px) {
+        grid-template-columns: repeat(2, 230px);
     }
-    @media screen and (max-width: 830px) {
-        grid-template-columns: repeat(2, 1fr);
+    @media screen and (max-width: ${variables.MEDIA_SECOND_WIDTH}px) {
+        display: flex;
+        flex-direction: column;
     }
-    @media screen and (max-width: 540px) {
-        grid-template-columns: 1fr;
-        & > li {
-            width: 350px;
-        }
+`;
+
+const ObserveLi = styled.li`
+    width: 100%;
+    height: fit-content;
+    grid-column: 1 / span 4;
+    @media screen and (max-width: ${variables.MAX_WIDTH}px) {
+        grid-column: 1 / span 3;
     }
-    @media screen and (max-width: 400px) {
-        & > li {
-            width: 230px;
-        }
+    @media screen and (max-width: ${variables.MEDIA_FIRST_WIDTH}px) {
+        grid-column: 1 / span 2;
     }
 `;
 
 const Main = () => {
+    const observeTargetRef = useRef(null);
     const userData = useRecoilValue(currentUserState);
     const navigate = useNavigate();
 
-    const getCodeList = async () => {
+    const getCodeList = async (pageParam: DocumentData | undefined) => {
         if (userData) {
-            const codeQuery = query(
-                collection(db, `user/${userData.uid}/codes`),
-                orderBy("timestamp", "desc"),
-                limit(12) // 추후에 무한 스크롤 추가
-            );
+            const codeQuery =
+                pageParam !== undefined
+                    ? query(
+                          collection(db, `user/${userData.uid}/codes`),
+                          orderBy("timestamp", "desc"),
+                          limit(variables.CODE_LIMIT),
+                          startAfter(pageParam)
+                      )
+                    : query(
+                          collection(db, `user/${userData.uid}/codes`),
+                          orderBy("timestamp", "desc"),
+                          limit(variables.CODE_LIMIT)
+                      );
             const codeSnapshot = await getDocs(codeQuery);
-            return codeSnapshot.docs;
+            return {
+                data: codeSnapshot.docs,
+                size: codeSnapshot.size,
+                lastDocument: codeSnapshot.docs[codeSnapshot.docs.length - 1],
+            };
         }
     };
 
-    const { data: codeList = null } = useQuery(queryKeys.code, getCodeList);
+    const {
+        data: codeList = null,
+        hasNextPage,
+        fetchNextPage,
+    } = useInfiniteQuery(
+        queryKeys.code,
+        ({ pageParam }) => getCodeList(pageParam),
+        {
+            getNextPageParam: (lastPage) => {
+                if (lastPage) {
+                    if (lastPage.size < 12) {
+                        return null;
+                    } else {
+                        return lastPage.lastDocument;
+                    }
+                }
+            },
+        }
+    );
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            async ([entry], observer) => {
+                observer.unobserve(entry.target);
+                if (entry.isIntersecting && hasNextPage) {
+                    await fetchNextPage();
+                }
+                observer.observe(entry.target);
+            },
+            { threshold: 0.5 }
+        );
+        if (observeTargetRef.current) {
+            observer.observe(observeTargetRef.current);
+        }
+    }, [observeTargetRef, hasNextPage, fetchNextPage]);
 
     return userData ? (
         <MainWrapper>
             {codeList &&
-                (codeList.length > 0 ? (
+                (codeList.pages[0]?.data.length! > 0 ? (
                     <CodeList>
-                        {codeList.map((doc) => {
-                            const codeData = doc.data();
-                            return (
-                                <li key={doc.id}>
-                                    <CodeCard
-                                        id={doc.id}
-                                        title={codeData.title}
-                                        description={codeData.description}
-                                        tags={codeData.tag}
-                                        language={codeData.language}
-                                        date={codeData.timestamp.toDate()}
-                                    />
-                                </li>
-                            );
-                        })}
+                        <>
+                            {codeList.pages.map((page) => {
+                                const codePage = page?.data;
+                                return codePage?.map((doc) => {
+                                    const codeData = doc.data();
+                                    return (
+                                        <li key={doc.id} className="card">
+                                            <CodeCard
+                                                id={doc.id}
+                                                title={codeData.title}
+                                                description={
+                                                    codeData.description
+                                                }
+                                                tags={codeData.tag}
+                                                language={codeData.language}
+                                                date={codeData.timestamp.toDate()}
+                                            />
+                                        </li>
+                                    );
+                                });
+                            })}
+                            {hasNextPage && (
+                                <ObserveLi ref={observeTargetRef}>
+                                    <Spinner />
+                                </ObserveLi>
+                            )}
+                        </>
                     </CodeList>
                 ) : (
                     <>
