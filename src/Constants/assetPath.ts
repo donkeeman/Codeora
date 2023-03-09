a24df717fe501f6e3b8ba5fddaba54ce92d82910
donkeeman <doonkeeemaan@gmail.com>
2023-03-09 19:57:01 +0900

    Fix: 소셜 로그인 로고 파일명 수정

--- src/Constants/assetPath.ts
+++ src/Constants/assetPath.ts
@@ -3,6 +3,6 @@ export const logoPath = "/Assets/logo.png";
 export const backgroundPath = "/Assets/background.jpg"
 
 export const iconPaths = {
-    google: "/Assets/btn_google_dark_normal_ios.svg",
-    github: "/Assets/gitHub-Mark-Light-120px-plus.svg",
+    google: "/Assets/google.svg",
+    github: "/Assets/github.svg",
 };
